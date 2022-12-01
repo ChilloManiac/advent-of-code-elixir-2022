f3f9f928ce32965c5359d87d9860f6330fda60b4
Christoffer Noerbjerg <christoffer.noerbjerg@consultant.lego.com>
2022-12-01 07:42:12 +0100

    First day

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Day01 do
+  defp split_elves(binary) do
+    String.split(binary, "\n\n")
+  end
+
+  defp to_num_array(elves) do
+    Enum.map(elves, fn elf ->
+      Enum.map(elf, fn calories_string ->
+        {value, ""} = Integer.parse(calories_string)
+        value
+      end)
+    end)
+  end
+
   def part1(args) do
+    args
+    |> split_elves
+    |> Enum.map(fn elf -> String.split(elf) end)
+    |> to_num_array
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(1)
+    |> Enum.sum
   end
 
   def part2(args) do
+    args
+    |> split_elves
+    |> Enum.map(fn elf -> String.split(elf) end)
+    |> to_num_array
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum
   end
 end
