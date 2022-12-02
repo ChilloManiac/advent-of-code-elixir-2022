0b328ed2518dbf07b77ce80e9f12fc0d5cc6b9f5
Christoffer Noerbjerg <christoffer.noerbjerg@consultant.lego.com>
2022-12-02 11:13:14 +0100

    Thansk rolf

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -24,13 +24,13 @@ defmodule AdventOfCode.Day01 do
   end
 
   def part2(args) do
-    args
-    |> split_elves
-    |> Enum.map(fn elf -> String.split(elf) end)
-    |> to_num_array
-    |> Enum.map(&Enum.sum/1)
-    |> Enum.sort(:desc)
-    |> Enum.take(3)
-    |> Enum.sum
+    args # String
+    |> split_elves # List[Str]
+    |> Enum.map(fn elf -> String.split(elf) end) # List[List[Str]]
+    |> to_num_array # List[List[Int]]
+    |> Enum.map(&Enum.sum/1) # List[int]
+    |> Enum.sort(:desc) # List[int]
+    |> Enum.take(3) # List[int]
+    |> Enum.sum # Int
   end
 end
--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,56 @@
 defmodule AdventOfCode.Day02 do
   def part1(args) do
+    args
+    |> String.split("\n")
+    |> Enum.reject(& &1 == "")
+    |> Enum.map(&calculate_score/1)
+    |> Enum.sum()
+  end
+
+  defp calculate_score(round) do
+    score_from_choice(round) + score_from_outcome(round)
+  end
+
+  defp score_from_choice(<<_::binary-size(2)>> <> "X"), do: 1
+  defp score_from_choice(<<_::binary-size(2)>> <> "Y"), do: 2
+  defp score_from_choice(<<_::binary-size(2)>> <> "Z"), do: 3
+
+  defp score_from_outcome(<<them::binary-size(1), " ", me::binary-size(1)>>) do
+    case :binary.first(me) - :binary.first(them) do
+      win when win in [24, 21] -> 6
+      23 -> 3 
+      _ -> 0
+    end
   end
 
   def part2(args) do
+    args
+    |> String.split("\n")
+    |> Enum.reject(& &1 == "")
+    |> Enum.map(&calculate_score_two/1)
+    |> Enum.sum()
   end
+
+  defp calculate_score_two(round) do
+    score_from_choice_two(round) + score_from_outcome_two(round)
+  end
+
+  @matrix %{
+    "A" => [loose: 3, draw: 1, win: 2],
+    "B" => [loose: 1, draw: 2, win: 3],
+    "C" => [loose: 2, draw: 3, win: 1],
+  }
+
+  defp score_from_choice_two(round) do
+    case String.split(round) do
+      [choice, "X"] -> Map.get(@matrix, choice) |> Keyword.get(:loose)
+      [choice, "Y"] -> Map.get(@matrix, choice) |> Keyword.get(:draw)
+      [choice, "Z"] -> Map.get(@matrix, choice) |> Keyword.get(:win)
+    end 
+  end
+
+  defp score_from_outcome_two(<<_::binary-size(2)>> <> "X"), do: 0
+  defp score_from_outcome_two(<<_::binary-size(2)>> <> "Y"), do: 3
+  defp score_from_outcome_two(<<_::binary-size(2)>> <> "Z"), do: 6
+
 end
